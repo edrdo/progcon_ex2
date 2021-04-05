aa6c1961ee09746699862f81650075926c394d46
Eduardo Marques <edrdo@dcc.fc.up.pt>
2021-04-05 02:03:45 +0100

    removed redundant field

--- Stack2.java
+++ Stack2.java
@@ -6,11 +6,9 @@ public class Stack2<E> implements IStack<E> {
   private final LinkedList<E> elems;
   private final ReentrantLock lock;
   private final Condition notEmpty;
-  private int size;
 
   public Stack2() {
     elems = new LinkedList<>();
-    size = 0;
     lock = new ReentrantLock();
     notEmpty = lock.newCondition();
   }
